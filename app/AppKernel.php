1ae9aa28e1263b6134b021b2fc7c9bd11a1b99c5
DESKTOP-JK0KEQ9\Hamdi <hamdiii.g@gmail.com>
2017-04-26 17:15:50 +0200

    adding authentication to the API using FOSOauthServerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
       new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
       new Voryx\RESTGeneratorBundle\VoryxRESTGeneratorBundle(),
       new FOS\RestBundle\FOSRestBundle(),
+      new FOS\UserBundle\FOSUserBundle(),
+      new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
       new JMS\SerializerBundle\JMSSerializerBundle($this),
       new Nelmio\CorsBundle\NelmioCorsBundle(),
       new AppBundle\AppBundle(),
